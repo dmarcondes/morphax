3663e899e1f264541dd63ded835665367ebd7076
Diego Marcondes <diegormmarcondes@gmail.com>
2024-03-21 17:16:31 -0500

    wop layer

--- morphax/morph.py
+++ morphax/morph.py
@@ -53,8 +53,7 @@ def index_array(shape):
 #Apply W-operator with characteristic function f at pixel (i,j)
 def local_w_operator(x,f,l):
     def jit_w_operator(index):
-        x = jax.lax.dynamic_slice(x, (index[0], index[1]), (2*l + 1, 2*l + 1))
-        return f(x)
+        return f(jax.lax.dynamic_slice(x, (index[0], index[1]), (2*l + 1, 2*l + 1)))
     return jit_w_operator
 
 #Apply W-operator with characteristic function f
@@ -73,8 +72,7 @@ def w_operator(x,index_x,f,d):
 #Apply W-operator with characteristic function f given by nn at pixel (i,j)
 def local_w_operator_nn(x,forward,params,l):
     def jit_w_operator(index):
-        x = jax.lax.dynamic_slice(x, (index[0], index[1]), (2*l + 1, 2*l + 1))
-        return forward(x,params)
+        return forward(jax.lax.dynamic_slice(x, (index[0], index[1]), (2*l + 1, 2*l + 1)),params)
     return jit_w_operator
 
 #Apply W-operator with characteristic function f given by nn
