eb9d813d95fa4d8dd6547641ce245374298102d3
Diego Marcondes <diegormmarcondes@gmail.com>
2024-06-27 14:47:35 -0500

    update jit

--- morphax/dmnn.py
+++ morphax/dmnn.py
@@ -880,7 +880,7 @@ def step_slda(params,x,y,forward,lf,type,width,size,sample = True,neighbors = 8)
                 tmp_random = jax.random.choice(jax.random.PRNGKey(np.random.choice(range(1000000))),max_size ** 2,shape = (1,),p = tmp_prob)
                 rc = jnp.array([jnp.floor(tmp_random/max_size),tmp_random % max_size]).reshape((1,2)).astype(jnp.int32)
                 #Sample limit
-                lim = jnp.sum(par_l[node,:,rc[0,0],rc[0,1]])
+                lim = jnp.sum(par_l[node,:,rc[0,0],rc[0,1]]).reshape((1,1))
                 #Neighbor
                 nei = jnp.append(jnp.append(jnp.append(l.reshape((1,1)),node,1),lim,1),rc,1)
                 if hood is None:
