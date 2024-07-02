3c0baa094e95a0b48a1e2be15ae18bf85ae28f26
Diego Marcondes <diegormmarcondes@gmail.com>
2024-07-02 05:43:21 -0500

    experiment

--- dmnn_game_of_life_experiments.py
+++ dmnn_game_of_life_experiments.py
@@ -75,9 +75,10 @@ net[8] = dmnn.cdmnn(['supgen','sup'],[1024,1],[3,1],shape_x = (32,32),sample = T
 
 #Training each one
 results = list(range(len(net)))
-for i in range(len(net)):
+for i in [5]:#range(len(net)):
     print(i)
     results[i] = dmnn.train_dmnn(x,y,net[i],dmnn.MSE,xval = xval,yval = yval,sample = True,neighbors = 8,epochs = 25000,batches = 2,notebook = True,epoch_print= 100,epoch_store = 10)
     tmp_table = pd.DataFrame(np.array([results[i]['trace_epoch'],results[i]['trace_time'],results[i]['trace_loss'],results[i]['trace_val_loss']]).transpose(),columns = ['epoch','time','train_loss','val_loss'])
     tmp_table.to_csv('dmnn_gol_2bacthes' + str(i) + '.csv')
+    jnp.save("params_5.npy",results[i]['best_par'])
     del tmp_table
